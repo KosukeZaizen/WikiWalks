b4f7ae49b0ad249214ea1f681efbe9df29c04239
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-06-25 10:07:04 +0900

    improve SQL

--- WikiWalks/Controllers/WikiWalksController.cs
+++ WikiWalks/Controllers/WikiWalksController.cs
@@ -19,10 +19,10 @@ namespace RelatedPages.Controllers
             var result = con.ExecuteSelect(@"
 select category, count(*) as cnt 
 from (
-	select wordId, category, count(*) as cnt1 from Category as c 
+	select c.category from Category as c 
 	inner join WordReference as r 
 	on c.wordId = r.targetWordId 
-	group by wordId, category
+	group by c.wordId, c.category
 	having count(*) > 4
 ) as rel
 group by category 
