7285fadde73b5fd3309e216b2fbe2bb47c93411e
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-08-30 10:36:25 +0900

    デプロイ時に重いSQLが２つ同時実行されることを防止

--- WikiWalks/Startup.cs
+++ WikiWalks/Startup.cs
@@ -41,17 +41,15 @@ namespace WikiWalks
             //一旦ロックを解除
             //DB_Util.releaseLock();
 
-            var allWorsGetter = new AllWorsGetter();
+            var allWorsGetter = new AllWordsGetter();
             services.AddSingleton(allWorsGetter);
-
-            System.Threading.Thread.Sleep(1000 * 10);//DBへの負荷を考慮してSleep
 
             var allCategoriesGetter = new AllCategoriesGetter(allWorsGetter);
             services.AddSingleton(allCategoriesGetter);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AllWorsGetter allWorsGetter, AllCategoriesGetter allCategoriesGetter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AllWordsGetter allWorsGetter, AllCategoriesGetter allCategoriesGetter)
         {
             if (env.IsDevelopment())
             {
@@ -164,15 +162,11 @@ namespace WikiWalks
         }
     }
 
-    public class AllWorsGetter
+    public class AllWordsGetter
     {
         private List<Page> pages = new List<Page>();
         private List<Page> newPages = new List<Page>();
         private int randomLimit = 5;
-        public AllWorsGetter()
-        {
-            Task.Run(() => hurryToSetAllPages());
-        }
 
         public IEnumerable<Page> getPages()
         {
@@ -193,7 +187,7 @@ namespace WikiWalks
             pages = pages.OrderByDescending(p => p.referenceCount).ToList();
         }
 
-        private void hurryToSetAllPages()
+        public void hurryToSetAllPages()
         {
             try
             {
@@ -364,15 +358,19 @@ from (
     public class AllCategoriesGetter
     {
         private IEnumerable<Category> categories = new List<Category>();
-        private AllWorsGetter allWorsGetter;
+        private AllWordsGetter allWordsGetter;
 
-        public AllCategoriesGetter(AllWorsGetter allWorsGetter)
+        public AllCategoriesGetter(AllWordsGetter allWordsGetter)
         {
             try
             {
-                this.allWorsGetter = allWorsGetter;
+                this.allWordsGetter = allWordsGetter;
 
-                Task.Run(() => hurryToSetAllCategories());
+                Task.Run(() => {
+                    allWordsGetter.hurryToSetAllPages();
+                    System.Threading.Thread.Sleep(1000 * 5);//DBへの負荷を考慮して5秒Sleep
+                    hurryToSetAllCategories();
+                });
 
                 Task.Run(async () =>
                 {
@@ -386,7 +384,7 @@ from (
                         {
                             try
                             {
-                                await allWorsGetter.setAllPagesAsync();
+                                await allWordsGetter.setAllPagesAsync();
                             }
                             catch (Exception ex)
                             {
@@ -413,7 +411,6 @@ from (
                             {
                                 //
                             }
-
                         }
                     }
                 });
@@ -425,8 +422,8 @@ from (
         {
             using (var client = new HttpClient())
             {
-                var response = await client.GetAsync(@"https://wiki-bat.azurewebsites.net/");
-                var msg = await response.Content.ReadAsStringAsync();
+                HttpResponseMessage response = await client.GetAsync(@"https://wiki-bat.azurewebsites.net/");
+                string msg = await response.Content.ReadAsStringAsync();
             }
         }
 
@@ -490,7 +487,7 @@ INSERT INTO Log VALUES (
             var con = new DBCon();
             var l = new List<Category>();
 
-            var pages = allWorsGetter.getPages().ToList();
+            var pages = allWordsGetter.getPages().ToList();
 
             var hashCategories = new HashSet<string>();
             foreach (var page in pages)
