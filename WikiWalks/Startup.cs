f02b0c737fbfb6d3f0f4df8c36b0f4bb6be913cf
Kosuke_Zaizen <kosuke.zaizen@infodex.co.jp>
2020-11-02 16:17:46 +0900

    デバッグ立ち上げ時にPagesの取得に失敗するのを防ぐ

--- WikiWalks/Startup.cs
+++ WikiWalks/Startup.cs
@@ -256,7 +256,7 @@ INSERT INTO Log VALUES (
                 {
                     using (var client = new HttpClient())
                     {
-                        var res = await client.GetAsync(@"https://wiki.lingual-ninja.com/api/WikiWalks/getPartialWords?num=10000");
+                        var res = await client.GetAsync(@"https://wiki.lingual-ninja.com/api/WikiWalks/getPartialWords?num=5000");
                         var result = await res.Content.ReadAsStringAsync();
                         pages = JsonConvert.DeserializeObject<List<Page>>(result);
 
